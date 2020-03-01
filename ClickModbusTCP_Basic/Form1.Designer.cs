707f7ad685e41f095496d43df2ae15f7a91c3b1e
Wylo <53132616+Wylo@users.noreply.github.com>
2020-02-29 17:37:08 -0800

    Add files via upload

---
+++ ClickModbusTCP_Basic/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace ClickModbusTCP_Basic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnC100 = new System.Windows.Forms.Button();
+            this.tbIpAddress = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblIP = new System.Windows.Forms.Label();
+            this.btnC101 = new System.Windows.Forms.Button();
+            this.btnC102 = new System.Windows.Forms.Button();
+            this.btnC104 = new System.Windows.Forms.Button();
+            this.btnC103 = new System.Windows.Forms.Button();
+            this.btnC106 = new System.Windows.Forms.Button();
+            this.btnC105 = new System.Windows.Forms.Button();
+            this.btnC113 = new System.Windows.Forms.Button();
+            this.btnC112 = new System.Windows.Forms.Button();
+            this.btnC111 = new System.Windows.Forms.Button();
+            this.btnC110 = new System.Windows.Forms.Button();
+            this.btnC109 = new System.Windows.Forms.Button();
+            this.btnC108 = new System.Windows.Forms.Button();
+            this.btnC107 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnC100
+            // 
+            this.btnC100.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnC100.Location = new System.Drawing.Point(14, 61);
+            this.btnC100.Name = "btnC100";
+            this.btnC100.Size = new System.Drawing.Size(103, 84);
+            this.btnC100.TabIndex = 0;
+            this.btnC100.Text = "C100";
+            this.btnC100.UseVisualStyleBackColor = true;
+            this.btnC100.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnC100_MouseDown);
+            this.btnC100.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnC100_MouseUp);
+            // 
+            // tbIpAddress
+            // 
+            this.tbIpAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbIpAddress.Location = new System.Drawing.Point(48, 12);
+            this.tbIpAddress.Name = "tbIpAddress";
+            this.tbIpAddress.Size = new System.Drawing.Size(150, 26);
+            this.tbIpAddress.TabIndex = 1;
+            this.tbIpAddress.Text = "10.10.10.10";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConnect.Location = new System.Drawing.Point(204, 7);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(73, 38);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIP.Location = new System.Drawing.Point(16, 15);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(26, 20);
+            this.lblIP.TabIndex = 3;
+            this.lblIP.Text = "IP";
+            // 
+            // btnC101
+            // 
+            this.btnC101.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnC101.Location = new System.Drawing.Point(137, 61);
+            this.btnC101.Name = "btnC101";
+            this.btnC101.Size = new System.Drawing.Size(103, 84);
+            this.btnC101.TabIndex = 4;
+            this.btnC101.Text = "C101";
+            this.btnC101.UseVisualStyleBackColor = true;
+            this.btnC101.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnC101_MouseDown);
+            this.btnC101.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnC101_MouseUp);
+            // 
+            // btnC102
+            // 
+            this.btnC102.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnC102.Location = new System.Drawing.Point(260, 61);
+            this.btnC102.Name = "btnC102";
+            this.btnC102.Size = new System.Drawing.Size(103, 84);
+            this.btnC102.TabIndex = 5;
+            this.btnC102.Text = "C102";
+            this.btnC102.UseVisualStyleBackColor = true;
+            this.btnC102.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnC102_MouseDown);
+            this.btnC102.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnC102_MouseUp);
+            // 
+            // btnC104
+            // 
+            this.btnC104.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnC104.Location = new System.Drawing.Point(506, 61);
+            this.btnC104.Name = "btnC104";
+            this.btnC104.Size = new System.Drawing.Size(103, 84);
+            this.btnC104.TabIndex = 7;
+            this.btnC104.Text = "C104";
+            this.btnC104.UseVisualStyleBackColor = true;
+            this.btnC104.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnC104_MouseDown);
+            this.btnC104.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnC104_MouseUp);
+            // 
+            // btnC103
+            // 
+            this.btnC103.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnC103.Location = new System.Drawing.Point(383, 61);
+            this.btnC103.Name = "btnC103";
+            this.btnC103.Size = new System.Drawing.Size(103, 84);
+            this.btnC103.TabIndex = 6;
+            this.btnC103.Text = "C103";
+            this.btnC103.UseVisualStyleBackColor = true;
+            this.btnC103.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnC103_MouseDown);
+            this.btnC103.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnC103_MouseUp);
+            // 
+            // btnC106
+            // 
+            this.btnC106.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnC106.Location = new System.Drawing.Point(752, 61);
+            this.btnC106.Name = "btnC106";
+            this.btnC106.Size = new System.Drawing.Size(103, 84);
+            this.btnC106.TabIndex = 9;
+            this.btnC106.Text = "C106";
+            this.btnC106.UseVisualStyleBackColor = true;
+            this.btnC106.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnC106_MouseDown);
+            this.btnC106.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnC106_MouseUp);
+            // 
+            // btnC105
+            // 
+            this.btnC105.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnC105.Location = new System.Drawing.Point(629, 61);
+            this.btnC105.Name = "btnC105";
+            this.btnC105.Size = new System.Drawing.Size(103, 84);
+            this.btnC105.TabIndex = 8;
+            this.btnC105.Text = "C105";
+            this.btnC105.UseVisualStyleBackColor = true;
+            this.btnC105.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnC105_MouseDown);
+            this.btnC105.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnC105_MouseUp);
+            // 
+            // btnC113
+            // 
+            this.btnC113.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnC113.Location = new System.Drawing.Point(752, 168);
+            this.btnC113.Name = "btnC113";
+            this.btnC113.Size = new System.Drawing.Size(103, 84);
+            this.btnC113.TabIndex = 16;
+            this.btnC113.Text = "C113";
+            this.btnC113.UseVisualStyleBackColor = true;
+            this.btnC113.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnC113_MouseDown);
+            this.btnC113.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnC113_MouseUp);
+            // 
+            // btnC112
+            // 
+            this.btnC112.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnC112.Location = new System.Drawing.Point(629, 168);
+            this.btnC112.Name = "btnC112";
+            this.btnC112.Size = new System.Drawing.Size(103, 84);
+            this.btnC112.TabIndex = 15;
+            this.btnC112.Text = "C112";
+            this.btnC112.UseVisualStyleBackColor = true;
+            this.btnC112.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnC112_MouseDown);
+            this.btnC112.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnC112_MouseUp);
+            // 
+            // btnC111
+            // 
+            this.btnC111.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnC111.Location = new System.Drawing.Point(506, 168);
+            this.btnC111.Name = "btnC111";
+            this.btnC111.Size = new System.Drawing.Size(103, 84);
+            this.btnC111.TabIndex = 14;
+            this.btnC111.Text = "C111";
+            this.btnC111.UseVisualStyleBackColor = true;
+            this.btnC111.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnC111_MouseDown);
+            this.btnC111.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnC111_MouseUp);
+            // 
+            // btnC110
+            // 
+            this.btnC110.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnC110.Location = new System.Drawing.Point(383, 168);
+            this.btnC110.Name = "btnC110";
+            this.btnC110.Size = new System.Drawing.Size(103, 84);
+            this.btnC110.TabIndex = 13;
+            this.btnC110.Text = "C110";
+            this.btnC110.UseVisualStyleBackColor = true;
+            this.btnC110.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnC110_MouseDown);
+            this.btnC110.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnC110_MouseUp);
+            // 
+            // btnC109
+            // 
+            this.btnC109.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnC109.Location = new System.Drawing.Point(260, 168);
+            this.btnC109.Name = "btnC109";
+            this.btnC109.Size = new System.Drawing.Size(103, 84);
+            this.btnC109.TabIndex = 12;
+            this.btnC109.Text = "C109";
+            this.btnC109.UseVisualStyleBackColor = true;
+            this.btnC109.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnC109_MouseDown);
+            this.btnC109.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnC109_MouseUp);
+            // 
+            // btnC108
+            // 
+            this.btnC108.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnC108.Location = new System.Drawing.Point(137, 168);
+            this.btnC108.Name = "btnC108";
+            this.btnC108.Size = new System.Drawing.Size(103, 84);
+            this.btnC108.TabIndex = 11;
+            this.btnC108.Text = "C108";
+            this.btnC108.UseVisualStyleBackColor = true;
+            this.btnC108.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnC108_MouseDown);
+            this.btnC108.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnC108_MouseUp);
+            // 
+            // btnC107
+            // 
+            this.btnC107.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnC107.Location = new System.Drawing.Point(14, 168);
+            this.btnC107.Name = "btnC107";
+            this.btnC107.Size = new System.Drawing.Size(103, 84);
+            this.btnC107.TabIndex = 10;
+            this.btnC107.Text = "C107";
+            this.btnC107.UseVisualStyleBackColor = true;
+            this.btnC107.MouseDown += new System.Windows.Forms.MouseEventHandler(this.btnC107_MouseDown);
+            this.btnC107.MouseUp += new System.Windows.Forms.MouseEventHandler(this.btnC107_MouseUp);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 277);
+            this.Controls.Add(this.btnC113);
+            this.Controls.Add(this.btnC112);
+            this.Controls.Add(this.btnC111);
+            this.Controls.Add(this.btnC110);
+            this.Controls.Add(this.btnC109);
+            this.Controls.Add(this.btnC108);
+            this.Controls.Add(this.btnC107);
+            this.Controls.Add(this.btnC106);
+            this.Controls.Add(this.btnC105);
+            this.Controls.Add(this.btnC104);
+            this.Controls.Add(this.btnC103);
+            this.Controls.Add(this.btnC102);
+            this.Controls.Add(this.btnC101);
+            this.Controls.Add(this.lblIP);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.tbIpAddress);
+            this.Controls.Add(this.btnC100);
+            this.Name = "Form1";
+            this.Text = "Basic Click Interface";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnC100;
+        private System.Windows.Forms.TextBox tbIpAddress;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.Button btnC101;
+        private System.Windows.Forms.Button btnC102;
+        private System.Windows.Forms.Button btnC104;
+        private System.Windows.Forms.Button btnC103;
+        private System.Windows.Forms.Button btnC106;
+        private System.Windows.Forms.Button btnC105;
+        private System.Windows.Forms.Button btnC113;
+        private System.Windows.Forms.Button btnC112;
+        private System.Windows.Forms.Button btnC111;
+        private System.Windows.Forms.Button btnC110;
+        private System.Windows.Forms.Button btnC109;
+        private System.Windows.Forms.Button btnC108;
+        private System.Windows.Forms.Button btnC107;
+    }
+}
+
---
+++ ClickModbusTCP_Basic/Form1.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EasyModbus;
+
+namespace ClickModbusTCP_Basic
+{
+    public partial class Form1 : Form
+    {
+        ModbusClient modbusClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            modbusClient = new ModbusClient();
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (modbusClient.Connected)
+                {
+                    modbusClient.Disconnect();
+                }
+
+
+                modbusClient.IPAddress = tbIpAddress.Text;
+                modbusClient.Port = 502;
+                modbusClient.SerialPort = null;
+
+                modbusClient.Connect();
+
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Unable to connect to PLC", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void WriteToPLC(int address, bool writeValue)
+        {
+            try
+            {
+                modbusClient.WriteSingleCoil(address - 1, writeValue);
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Exception writing values to Server", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        #region Button Clicks
+        private void btnC100_MouseDown(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16484, true);
+        }
+
+        private void btnC100_MouseUp(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16484, false);
+        }
+
+        private void btnC101_MouseDown(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16485, true);
+        }
+
+        private void btnC101_MouseUp(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16485, false);
+        }
+
+        private void btnC102_MouseDown(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16486, true);
+        }
+
+        private void btnC102_MouseUp(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16486, false);
+        }
+
+        private void btnC103_MouseDown(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16487, true);
+        }
+
+        private void btnC103_MouseUp(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16487, false);
+        }
+
+        private void btnC104_MouseDown(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16488, true);
+        }
+
+        private void btnC104_MouseUp(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16488, false);
+        }
+
+        private void btnC105_MouseDown(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16489, true);
+        }
+
+        private void btnC105_MouseUp(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16489, false);
+        }
+
+        private void btnC106_MouseDown(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16490, true);
+        }
+
+        private void btnC106_MouseUp(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16490, false);
+
+        }
+
+        private void btnC107_MouseDown(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16491, true);
+        }
+
+        private void btnC107_MouseUp(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16491, false);
+
+        }
+
+        private void btnC108_MouseDown(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16492, true);
+        }
+
+        private void btnC108_MouseUp(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16492, false);
+
+        }
+
+        private void btnC109_MouseDown(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16493, true);
+        }
+
+        private void btnC109_MouseUp(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16493, false);
+
+        }
+
+        private void btnC110_MouseDown(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16494, true);
+        }
+
+        private void btnC110_MouseUp(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16494, false);
+
+        }
+
+        private void btnC111_MouseDown(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16495, true);
+        }
+
+        private void btnC111_MouseUp(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16495, false);
+
+        }
+
+        private void btnC112_MouseDown(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16496, true);
+        }
+
+        private void btnC112_MouseUp(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16496, false);
+
+        }
+
+        private void btnC113_MouseDown(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16497, true);
+        }
+
+        private void btnC113_MouseUp(object sender, MouseEventArgs e)
+        {
+            WriteToPLC(16497, false);
+
+        }
+
+        #endregion
+    }
+}
